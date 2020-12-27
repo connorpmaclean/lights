bbd897298ab6176c5eb461b84122e1ee8a489a3f
Connor MacLean <connorpatrickmaclean@gmail.com>
2020-12-26 17:22:51 -0800

    Simplify trigger logic

--- AzureFunction/Timer.cs
+++ AzureFunction/Timer.cs
@@ -4,7 +4,6 @@ namespace Lights.Function
     using System;
     using System.Threading.Tasks;
     using Microsoft.Azure.WebJobs;
-    using Microsoft.Azure.WebJobs.Host;
     using Microsoft.Extensions.Logging;
 
     using Lights.Core;
@@ -13,15 +12,8 @@ namespace Lights.Function
     {
         // Set WEBSITE_TIME_ZONE to Pacific Standard Time
         // func azure functionapp publish cmlights --csharp
-        [FunctionName("LightsPrime")]
-        public static async Task LightsPrime([TimerTrigger("*/5 * 16-23 * * *")]TimerInfo myTimer, ILogger log)
-        {
-            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-            await LightsCore.Run(log);
-        }
-
         [FunctionName("LightsOfftime")]
-        public static async Task LightsOfftime([TimerTrigger("*/30 * 0-15 * * *")]TimerInfo myTimer, ILogger log)
+        public static async Task LightsOfftime([TimerTrigger("*/30 * * * * *")]TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
             await LightsCore.Run(log);
